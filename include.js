069d075f0e0df652542130e2dc0521afbbf5c99d
Renaldi Dwi Nanda Prasetya <115177015+renaldottt@users.noreply.github.com>
2023-08-15 14:52:41 +0700

    include.js

--- include.js
+++ include.js
@@ -1,3 +1,11 @@
 function gotoLogin() {
-alert("Oke")
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+    if(
+        username == "admin" && password == "admin"){
+            alert("oke")
+        }
+        else (
+            alert ("gagal")
+        )
 }
